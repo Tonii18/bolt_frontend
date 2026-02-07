fb9c5259375b12050c85604aff27528fff023eab
Jesus <gomez.rojes22@cadiz.salesianos.edu>
2026-02-07 17:02:48 +0100

    Change to task admin model

--- lib/models/task_admin.dart
+++ lib/models/task_admin.dart
@@ -1,20 +1,26 @@
 class TaskAdmin {
+  final String id;
   final String title;
   final String description;
+  final String state;
 
-  TaskAdmin({required this.title, required this.description});
+  TaskAdmin({required this.id, required this.title, required this.description, required this.state});
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'title': title,
       'description': description,
+      'state': state,
     };
   }
 
   factory TaskAdmin.fromJson(Map<String, dynamic> json) {
     return TaskAdmin(
+      id: json['id'],
       title: json['title'],
       description: json['description'],
+      state: json['state'],
     );
   }
 }
